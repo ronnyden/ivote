7d80c2944452d0573d70d0d494a831f2d79fc22a
ronnyden <dennisor98@gmail.com>
2021-09-13 12:25:50 -0400

    voter registration update

--- app/Http/Controllers/Voters.php
+++ app/Http/Controllers/Voters.php
@@ -8,23 +8,29 @@ use Illuminate\Http\Request;
 class Voters extends Controller
 {
     public function store(Request $request){
-        $request->validate([
-          'firstname'=>'required|string|max:255',
-          'lastname'=>'required|string|max:255',
-          'admissionNumber'=>'required|string|unique',
-          'email'=>'required|string|email|unique'
+        // $request->validate([
+        //   'firstname'=>'required|string|max:255',
+        //   'lastname'=>'required|string|max:255',
+        //   'admissionNumber'=>'required|string|unique',
+        //   'email'=>'required|string|email|unique'
 
-        ]);
-    Voter::create(
-        [
-            'firstname'=>$request['firstname'],
-            'lastname'=> $request['lastname'],
-            'admissionNumber'=> $request['admissionNumber'],
-            'email'=> $request['email']
-        ]
-        );
+        // ]);
+    $voter = new Voter;
+            $voter->first_name = $request['firstname'];
+            $voter->last_name = $request['lastname'];
+            $voter->admission_number = $request['admissionNumber'];
+            $voter->email_address = $request['email'];
+
+            $voter->save();
+        
+        
         return response("Voter registered",201);
 
+    }
+
+    public function show($id){
+        $voter = Voter::find($id);
+        return response($voter);
     }
 
     public function update(Request $request,$id){
