1c9d3098d0d887c5744e8e4f8c7e9ed059f7fab6
ronnyden <dennisor98@gmail.com>
2021-09-14 11:20:05 -0400

    Voter model update

--- app/Http/Controllers/VoteProcess.php
+++ app/Http/Controllers/VoteProcess.php
@@ -4,9 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Candidate;
+use App\Models\Position;
 
 class VoteProcess extends Controller
 {
+
+    public function registerCandidate(Request $request){
+        $request->validate([
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|email|unique:candidates'
+        ]);
+        $candidate = new Candidate;
+        $candidate->first_name = $request['first_name'];
+        $candidate->last_name = $request['last_name'];
+        $candidate->email = $request['email_address'];
+        $candidate->position = $request['position'];
+        $candidate->save();
+    }
+
+    public function getPositions(){
+        $pos = Position::findAll();
+        return response($pos);
+
+    }
     public function processVote($id){
         $candidate = Candidate::find($id);
         $candidate->update(['votes'=>($candidate->votes + 1)]);
