0397cef0a417922022ddc797248aa54e3a021e2d
ronnyden <dennisor98@gmail.com>
2021-09-15 02:45:03 -0400

    routing update

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\VoteProcess;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Voters;
+use App\Http\Controllers\Candidates;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -33,10 +34,10 @@ Route::get('/admin/logout',[Admins::class,'logout']);
   |Candidate endpoints
   |_____________________________________________________
 */
-Route::post('/candidate/register',[Candidates::class,'store']);
+Route::post('/candidate/register',[Candidates::class,'registerCandidate']);
 Route::put('/candidate/update/{id}',[Candidates::class,'update']);
-Route::get("/candidate/{id}",[Candidates::class,'show']);
-Route::delete('candidate/delete/{id}',[Candidates::class,'destroy']);
+Route::get("/candidate/{id}",[Candidates::class,'findCandidate']);
+Route::delete('candidate/delete/{id}',[Candidates::class,'deleteCandidate']);
 Route::get('/candidate/login',[Candidates::class,'login']);
 Route::get('/candidate/logout',[Candidates::class,'logout']);
 /*_________________________________________________________
@@ -49,5 +50,6 @@ Route::get('/voter/list',[Voters::class,'index']);
 Route::get('/voter/{id}',[Voters::class,'show']);
 Route::put("/voter/update/id",[Voters::class,'update']);
 Route::delete('/voter/delete/{id}',[Voters::class,'destroy']);
-
+Route::post('/voter/login',[Voters::class,'login']);
+Route::post('/voter/logout',[Voters::class,'logout']);
 Route::post('/vote/{id}',[VoteProcess::class,'processVote']);
