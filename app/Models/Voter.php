ad9dc65032d24da263d5dbb8ccca8d94ee9d3286
ronnyden <dennisor98@gmail.com>
2021-09-15 01:21:53 -0400

    voter model update

--- app/Models/Voter.php
+++ app/Models/Voter.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use App\Http\Middleware\Authenticate;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Voter extends Model
+class Voter extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory,HasApiTokens,Notifiable;
 }
